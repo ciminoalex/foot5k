e9266758adbf2878f7710e8b4195ef9ea0fbf6f9
Alessandro Cimino <cimino.alex@gmail.com>
2017-11-10 11:42:25 +0100

    UPLOAD IMAGE

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -47,6 +47,8 @@ export class SettingsPage {
   profile: ProfileModel = new ProfileModel();
   languages: Array<LanguageModel>;
 
+  lastImage: string = null;
+
   public CurrentUser: UserObject;
 
   constructor(
@@ -251,15 +253,83 @@ export class SettingsPage {
           .then(filePath => {
             let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
             let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
-            //this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
           });
       } else {
         var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
         var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-        //this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
       }
     }, (err) => {
-      //this.presentToast('Error while selecting image.');
+      this.presentToast('Error while selecting image.');
+    });
+  }
+
+  private createFileName() {
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  n + ".jpg";
+    return newFileName;
+  }
+
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      this.lastImage = newFileName;
+    }, error => {
+      this.presentToast('Error while storing file.');
+    });
+  }
+
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      return cordova.file.dataDirectory + img;
+    }
+  }
+
+
+  public uploadImage() {
+    // Destination URL
+    var url = "http://foot5k.com/upload.php";
+   
+    // File for Upload
+    var targetPath = this.pathForImage(this.lastImage);
+   
+    // File name only
+    var filename = this.lastImage;
+   
+    var options = {
+      fileKey: "file",
+      fileName: filename,
+      chunkedMode: false,
+      mimeType: "multipart/form-data",
+      params : {'fileName': filename}
+    };
+   
+    const fileTransfer: TransferObject = this.transfer.create();
+   
+    this.loading = this.loadingCtrl.create({
+      content: 'Uploading...',
+    });
+    this.loading.present();
+   
+    // Use the FileTransfer to upload the image
+    fileTransfer.upload(targetPath, url, options).then(data => {
+      this.loading.dismissAll()
+      this.presentToast('Image succesful uploaded.');
+    }, err => {
+      this.loading.dismissAll()
+      this.presentToast('Error while uploading file.');
     });
   }
 
